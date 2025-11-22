a1d75a9861d98dc84b3f46a00ebe481b701df93e
MihailK <mihailkopitov@mail.ru>
2025-11-22 15:19:17 +0500

    Отрефакторил Serializer

---
+++ ObjectPrinting/ObjectPrinter/SerializationContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectPrinting.ObjectPrinter;
+
+internal class SerializationContext<TOwner>(PrintingConfig<TOwner> config, StringBuilder builder)
+{
+    public readonly PrintingConfig<TOwner> Config = config;
+    public readonly HashSet<object> Visited = new(ReferenceEqualityComparer.Instance);
+    public readonly StringBuilder Builder = builder;
+
+    public void Indent(int level) => Builder.Append(new string('\t', level));
+}
+
+
--- ObjectPrinting/ObjectPrinter/Serializer.cs
+++ ObjectPrinting/ObjectPrinter/Serializer.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace ObjectPrinting;
+namespace ObjectPrinting.ObjectPrinter;
 
-internal sealed class Serializer<TOwner>
+internal static class Serializer
 {
-    private readonly PrintingConfig<TOwner> config;
-    private readonly HashSet<object> visited = new(ReferenceEqualityComparer.Instance);
-
-    private static readonly Type[] TerminalTypes =
+    private static readonly HashSet<Type> TerminalTypes =
     [
         typeof(string), typeof(DateTime), typeof(TimeSpan),
         typeof(Guid), typeof(decimal),
@@ -21,184 +19,243 @@ internal sealed class Serializer<TOwner>
         typeof(float), typeof(double), typeof(bool), typeof(char)
     ];
 
-    internal Serializer(PrintingConfig<TOwner> config) => this.config = config;
-
-    internal string SerializeObject(object? obj)
+    internal static string SerializeObject<TOwner>(TOwner? obj, PrintingConfig<TOwner> config)
     {
         var sb = new StringBuilder();
-        Serialize(obj, sb, 0, parentMember: null);
+        var ctx = new SerializationContext<TOwner>(config, sb);
+        Serialize(obj, 0, null, ctx);
         return sb.ToString();
     }
 
-    private void Serialize(object? obj, StringBuilder sb, int level, MemberInfo? parentMember)
+    private static void Serialize<TOwner>(
+        object? obj,
+        int level,
+        MemberInfo? parentMember,
+        SerializationContext<TOwner> ctx)
+    {
+        if (TryHandleNullOrExcludedOrDepth(obj, level, ctx))
+            return;
+
+        var type = obj!.GetType();
+
+        if (TryHandleCycle(obj, type, ctx))
+            return;
+
+        if (TryHandleCustomTypeSerializer(obj, type, parentMember, ctx))
+            return;
+
+        if (TryHandleTerminal(obj, type, parentMember, ctx))
+            return;
+
+        if (TryHandleDictionary(obj, type, level, ctx))
+            return;
+
+        if (TryHandleEnumerable(obj, type, level, ctx))
+            return;
+
+        SerializeComplexObject(obj, type, level, ctx);
+    }
+
+
+    private static bool TryHandleNullOrExcludedOrDepth<TOwner>(
+        object? obj,
+        int level,
+        SerializationContext<TOwner> ctx)
     {
         if (obj is null)
         {
-            sb.AppendLine("null");
-            return;
+            ctx.Builder.AppendLine("null");
+            return true;
         }
 
         var type = obj.GetType();
-        
-        if (config.ExcludedTypes.Contains(type))
-        {
-            sb.AppendLine("[Excluded Type]");
-            return;
-        }
-        
-        if (level >= config.MaxNestingLevel)
-        {
-            sb.AppendLine($"[{FormatTypeName(type)} превышен уровень вложенности]");
-            return;
-        }
-        
-        if (!type.IsValueType)
-        {
-            if (visited.Contains(obj))
-            {
-                sb.AppendLine($"[CyclicRef {FormatTypeName(type)}]");
-                return;
-            }
 
-            visited.Add(obj);
+        if (ctx.Config.ExcludedTypes.Contains(type))
+        {
+            ctx.Builder.AppendLine("[Excluded Type]");
+            return true;
         }
 
-
-        if (config.CustomTypeSerializers.TryGetValue(type, out var typeSer))
+        if (level >= ctx.Config.MaxNestingLevel)
         {
-            var text = InvokeSerializer(typeSer, obj);
-            sb.Append(ApplyTrimming(text, parentMember));
-            return;
+            ctx.Builder.AppendLine($"[{FormatTypeName(type)} превышен уровень вложенности]");
+            return true;
         }
 
+        return false;
+    }
 
-        if (TerminalTypes.Contains(type) || type.IsEnum)
+    private static bool TryHandleCycle<TOwner>(
+        object obj,
+        Type type,
+        SerializationContext<TOwner> ctx)
+    {
+        if (type.IsValueType)
+            return false;
+
+        if (ctx.Visited.Add(obj)) 
+            return false;
+        ctx.Builder.AppendLine($"[CyclicRef {FormatTypeName(type)}]");
+        return true;
+
+    }
+
+    private static bool TryHandleCustomTypeSerializer<TOwner>(
+        object obj,
+        Type type,
+        MemberInfo? parentMember,
+        SerializationContext<TOwner> ctx)
+    {
+        if (!ctx.Config.CustomTypeSerializers.TryGetValue(type, out var typeSer))
+            return false;
+
+        var text = typeSer(obj);
+        ctx.Builder.Append(ApplyTrimming(text, parentMember, ctx.Config));
+        return true;
+    }
+
+    private static bool TryHandleTerminal<TOwner>(
+        object obj,
+        Type type,
+        MemberInfo? parentMember,
+        SerializationContext<TOwner> ctx)
+    {
+        if (!TerminalTypes.Contains(type) && !type.IsEnum)
+            return false;
+
+        ctx.Builder.Append(FormatScalar(obj, type, parentMember, ctx.Config));
+        return true;
+    }
+
+    private static bool TryHandleDictionary<TOwner>(
+        object obj,
+        Type type,
+        int level,
+        SerializationContext<TOwner> ctx)
+    {
+        if (obj is not IDictionary dict)
+            return false;
+
+        ctx.Builder.AppendLine(FormatTypeName(type));
+        foreach (DictionaryEntry entry in dict)
         {
-            sb.Append(FormatScalar(obj, type, parentMember));
-            return;
+            ctx.Indent(level + 1);
+            ctx.Builder.Append("Key = ");
+            Serialize(entry.Key, level + 1, null, ctx);
+
+            ctx.Indent(level + 1);
+            ctx.Builder.Append("Value = ");
+            Serialize(entry.Value, level + 1, null, ctx);
         }
-        
-        if (obj is IDictionary dict)
+
+        return true;
+    }
+
+    private static bool TryHandleEnumerable<TOwner>(
+        object obj,
+        Type type,
+        int level,
+        SerializationContext<TOwner> ctx)
+    {
+        if (obj is not IEnumerable enumerable || type == typeof(string))
+            return false;
+
+        ctx.Builder.AppendLine(FormatTypeName(type));
+        var i = 0;
+        foreach (var item in enumerable)
         {
-            sb.AppendLine(FormatTypeName(type));
-            foreach (DictionaryEntry entry in dict)
-            {
-                Indent(sb, level + 1);
-                sb.Append("Key = ");
-                Serialize(entry.Key, sb, level + 1, null);
-
-                Indent(sb, level + 1);
-                sb.Append("Value = ");
-                Serialize(entry.Value, sb, level + 1, null);
-            }
-
-            return;
+            ctx.Indent(level + 1);
+            ctx.Builder.Append($"[{i}] = ");
+            Serialize(item, level + 1, null, ctx);
+            i++;
         }
-        
-        if (obj is IEnumerable enumerable && type != typeof(string))
-        {
-            sb.AppendLine(FormatTypeName(type));
-            var i = 0;
-            foreach (var item in enumerable)
-            {
-                Indent(sb, level + 1);
-                sb.Append($"[{i}] = ");
-                Serialize(item, sb, level + 1, null);
-                i++;
-            }
 
-            return;
-        }
-        
-        sb.AppendLine(FormatTypeName(type));
+        return true;
+    }
+
+    private static void SerializeComplexObject<TOwner>(
+        object obj,
+        Type type,
+        int level,
+        SerializationContext<TOwner> ctx)
+    {
+        ctx.Builder.AppendLine(FormatTypeName(type));
 
         foreach (var prop in type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
         {
             if (!prop.CanRead) continue;
-            if (config.ExcludedMember.Contains(prop)) continue;
-            if (config.ExcludedTypes.Contains(prop.PropertyType)) continue;
 
-            Indent(sb, level + 1);
-            sb.Append(prop.Name);
-            sb.Append(" = ");
-
-            if (config.CustomMemberSerializers.TryGetValue(prop, out var memberSer))
-            {
-                var value = SafeGet(() => prop.GetValue(obj));
-                var text = value is null ? "null" : InvokeSerializer(memberSer, value!);
-                sb.AppendLine(ApplyTrimming(text, parentMember: prop).TrimEnd('\r', '\n'));
-                continue;
-            }
-
-            var propValue = SafeGet(() => prop.GetValue(obj));
-            Serialize(propValue, sb, level + 1, prop);
+            SerializeMember(prop, level,
+                p => p.PropertyType,
+                p => p.GetValue(obj),
+                p => p.Name,
+                ctx);
         }
 
         foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.Public))
         {
-            if (config.ExcludedMember.Contains(field)) continue;
-            if (config.ExcludedTypes.Contains(field.FieldType)) continue;
-
-            Indent(sb, level + 1);
-            sb.Append(field.Name);
-            sb.Append(" = ");
-
-            if (config.CustomMemberSerializers.TryGetValue(field, out var memberSer))
-            {
-                var value = SafeGet(() => field.GetValue(obj));
-                var text = value is null ? "null" : InvokeSerializer(memberSer, value!);
-                sb.AppendLine(ApplyTrimming(text, parentMember: field).TrimEnd('\r', '\n'));
-                continue;
-            }
-
-            var fieldValue = SafeGet(() => field.GetValue(obj));
-            Serialize(fieldValue, sb, level + 1, field);
+            SerializeMember(field, level,
+                f => f.FieldType,
+                f => f.GetValue(obj),
+                f => f.Name,
+                ctx);
         }
     }
 
 
-    private static void Indent(StringBuilder sb, int level)
-        => sb.Append(new string('\t', level));
-    
-
-    private string FormatScalar(object value, Type type, MemberInfo? parentMember)
+    private static string FormatScalar<TOwner>(
+        object value,
+        Type type,
+        MemberInfo? parentMember,
+        PrintingConfig<TOwner> config)
     {
         if (config.CustomTypeSerializers.TryGetValue(type, out var del))
         {
-            var s = InvokeSerializer(del, value);
-            return ApplyTrimming(s, parentMember);
+            var s = del(value);
+            return ApplyTrimming(s, parentMember, config);
         }
+
+        string raw;
 
         if (type == typeof(string))
-        {
-            var s = (string)value;
-            return ApplyTrimming(s, parentMember);
-        }
-        
-        if (!config.CulturesForTypes.TryGetValue(type, out var culture))
-            return value + Environment.NewLine;
-        if (value is IFormattable fmt)
-            return fmt.ToString(null, culture) + Environment.NewLine;
+            raw = (string)value;
+        else if (config.CulturesForTypes.TryGetValue(type, out var culture) && value is IFormattable fmt)
+            raw = fmt.ToString(null, culture);
+        else
+            raw = value.ToString() ?? string.Empty;
 
-        return value + Environment.NewLine;
+        return ApplyTrimming(raw, parentMember, config);
     }
 
-    private string ApplyTrimming(string s, MemberInfo? parentMember)
+    private static string ApplyTrimming<TOwner>(
+        string? s,
+        MemberInfo? parentMember,
+        PrintingConfig<TOwner> config)
     {
         int? length = null;
-        if (parentMember != null && config.TrimmedMembers.TryGetValue(parentMember, out var memberLen))
+
+        if (parentMember != null &&
+            config.TrimmedMembers.TryGetValue(parentMember, out var memberLen))
+        {
             length = memberLen;
-        else if (config.TrimStringLength.HasValue)
-            length = config.TrimStringLength;
+        }
+        else if (parentMember is PropertyInfo pi &&
+                 config.TrimmedTypes.TryGetValue(pi.PropertyType, out var typeLen))
+        {
+            length = typeLen;
+        }
+        else if (parentMember is FieldInfo fi &&
+                 config.TrimmedTypes.TryGetValue(fi.FieldType, out var fieldTypeLen))
+        {
+            length = fieldTypeLen;
+        }
 
         if (length.HasValue && s != null && s.Length > length.Value)
-            s = s.Substring(0, length.Value);
+            s = s[..length.Value];
 
+        Debug.Assert(s != null, nameof(s) + " != null");
         return s.EndsWith(Environment.NewLine) ? s : s + Environment.NewLine;
     }
-
-    private static string InvokeSerializer(Delegate del, object value)
-        => (string)del.DynamicInvoke(value)!;
 
     private static object? SafeGet(Func<object?> getter)
     {
@@ -211,24 +268,45 @@ internal sealed class Serializer<TOwner>
             return null;
         }
     }
-    
+
     private static string FormatTypeName(Type t)
     {
-        if (!t.IsGenericType) return t.Name;
+        if (!t.IsGenericType)
+            return t.Name;
 
         var defName = t.Name;
         var backtick = defName.IndexOf('`');
-        if (backtick > 0) defName = defName.Substring(0, backtick);
+        defName = defName[..backtick];
 
         var args = t.GetGenericArguments();
         var argNames = string.Join(", ", args.Select(FormatTypeName));
         return $"{defName}<{argNames}>";
     }
-    
-    private sealed class ReferenceEqualityComparer : IEqualityComparer<object>
+
+    private static void SerializeMember<TOwner, TMember>(
+        TMember member,
+        int level,
+        Func<TMember, Type> getMemberType,
+        Func<TMember, object?> getValue,
+        Func<TMember, string> getName,
+        SerializationContext<TOwner> ctx)
+        where TMember : MemberInfo
     {
-        public static readonly ReferenceEqualityComparer Instance = new();
-        public new bool Equals(object x, object y) => ReferenceEquals(x, y);
-        public int GetHashCode(object obj) => System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
+        if (ctx.Config.ExcludedMember.Contains(member)) return;
+        if (ctx.Config.ExcludedTypes.Contains(getMemberType(member))) return;
+
+        ctx.Indent(level + 1);
+        ctx.Builder.Append(getName(member)).Append(" = ");
+
+        if (ctx.Config.CustomMemberSerializers.TryGetValue(member, out var memberSer))
+        {
+            var value = SafeGet(() => getValue(member));
+            var text = value is null ? "null" : memberSer(value);
+            ctx.Builder.AppendLine(ApplyTrimming(text, member, ctx.Config).TrimEnd('\r', '\n'));
+            return;
+        }
+
+        var memberValue = SafeGet(() => getValue(member));
+        Serialize(memberValue, level + 1, member, ctx);
     }
 }
